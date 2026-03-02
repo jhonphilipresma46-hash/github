658070ff513d9a86163eaa0132ee6ed560bfc043
jhonphilipresma46-hash <jhonphilipresma46@gmail.com>
2026-03-02 17:18:19 +0800

    gfgfgfgfg

--- Activity1-5.C
+++ Activity1-5.C
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 int main()
-{
-  float arr2[5];
-  for (int i = 0; i < 5; i++)
-  {
-    arr2[i] = (float)i * 2.1;
-  }
-  printf("%2f\n", arr2[2]);
+
+{ 
+  int arr[14];
+  int sum = 0;
+  
+
+    for (int i=0; i<14; i++) 
+    {
+    arr[i] = i + 1;
+    }
+    for (int i=0; i<14; i++) 
+    {
+    sum+= arr[i];
+    }
+
+printf("The sum of all numbers from 1 to 14 is %d\n", sum);
+
+    return 0;
+  
 }
