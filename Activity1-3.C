a6de2ab39a6995c4d9dc99ff25370a83f4b3c682
jhonphilipresma46-hash <jhonphilipresma46@gmail.com>
2026-03-03 22:53:18 +0800

    dfjdfafkbkdbafkj

--- Activity1-3.C
+++ Activity1-3.C
@@ -1,3 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+int main() {
+    int arr[10];
+    int i, max;
+
+
+    srand(time(NULL));
+
+    
+    for(i = 0; i < 10; i++) {
+        arr[i] = rand() % 100 + 1;
+    }
+
+    
+    printf("Array elements:\n");
+    for(i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    
+    max = arr[0];
+    for(i = 1; i < 10; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("\n\nMaximum value: %d\n", max);
+
+    return 0;
+}
