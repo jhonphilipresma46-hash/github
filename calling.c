d79d5247a6b810ef697a7a9a818988bb6e1f99a9
jhonphilipresma46-hash <jhonphilipresma46@gmail.com>
2026-03-29 14:59:15 +0800

    222

---
+++ calling.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sum(int, int);
+int sum(int x, int y)
+{
+
+int sum; 
+sum = x + y;
+return x + y; 
+
+}
+int main()
+{ 
+int x = 10, y = 11;
+
+int result = sum(x, y);
+printf("sum of %d and %d = %d ", x , y, result);
+return 0;
+
+
+
+}
---
+++ function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void printMessage() { 
+    printf("Hello, I'm jhon\n");
+
+
+}
+
+int main() {
+    printMessage();
+    return 0;
+}
