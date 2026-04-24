197bcc4fb9de1676b3be3ee756570d8f89937b69
jhonphilipresma46-hash <jhonphilipresma46@gmail.com>
2026-04-24 18:11:44 +0800

    jjjjjjjjj

---
+++ POINTERBASICS.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+
+{ 
+    int a = 20; 
+    int *ptr;
+
+    ptr = &a;
+
+    printf("Value of a: %d\n", a);
+    printf("Address of a: %p\n", (void*)&a);
+    printf("Value stored in pointer: %p\n", (void*)ptr);
+    printf("Dereferenced value (*p): %d\n", *ptr);
+
+    return 0;
+
+
+
+
+
+}
